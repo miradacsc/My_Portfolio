d7ec4778b40240168d1c9d4e63f33fdaa0e825dd
Claudia S Cabrera <miradacsc@gmail.com>
2022-05-13 19:56:03 -0300

    dark mode + typing

--- js/style-switcher.js
+++ js/style-switcher.js
@@ -5,14 +5,18 @@ styleSwitcherToggle.addEventListener("click", () => {
     document.querySelector(".style-switcher").classList.toggle("open");
 })
 
+
 // hide style - switcher on scroll 
 window.addEventListener("scroll", () => {
-    if (document.querySelector(".style-switcher").classList.contains("open")) {
+    if(document.querySelector(".style-switcher").classList.contains("open")) {
         document.querySelector(".style-switcher").classList.remove("open");
     }
 })
 
+
 /* theme colors*/
+
+
 const alternateStyles = document.querySelectorAll(".alternate-style");
 function setActiveStyle(color) {
     alternateStyles.forEach((style) => {
@@ -23,3 +27,41 @@ function setActiveStyle(color) {
         }
     })
 }
+
+/* light and dark mode */
+
+const dayNight = document.querySelector(".day-night");
+
+dayNight.addEventListener("click", () => {
+    dayNight.querySelector("i").classList.toggle("fa-sun");
+    dayNight.querySelector("i").classList.toggle("fa-moon");
+    document.body.classList.toggle("dark");
+})
+window.addEventListener("load", () => {
+    if (document.body.classList.contains("dark")) {
+        dayNight.querySelector("i").classList.add("fa-sun");
+    } else {
+        dayNight.querySelector("i").classList.add("fa-moon");
+    }
+})
+
+
+
+
+
+
+
+/*
+const alternateStyle = Document.querySelectorAll(".alternate-style");
+function setActiveStyle(color) {
+    localStorage.setItem("color", color);
+    changeColor();
+}
+
+function changeColor() {
+    alternateStyle.forEach((style) => {
+        if (localStorage.getitem("color") === style.getAttribute("title")) {
+            style.removeAttribute("disabled");
+        }
+    })
+}*/
