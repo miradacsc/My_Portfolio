cf2578bb775cc1312f58f0481c0348d99615fafd
Claudia S Cabrera <miradacsc@gmail.com>
2022-05-14 00:44:58 -0300

    more javascript

--- js/script.js
+++ js/script.js
@@ -12,12 +12,18 @@ var typed = new Typed(".typing", {
 const nav = document.querySelector(".nav"),
     navList = nav.querySelectorAll("li"),
     totalNavList = navList.length;
-    //allSection = document.querySelectorAll(".section"),
-    //totalSection = allSection.length;    
+    allSection = document.querySelectorAll(".section");
+    totalSection = allSection.length;    
     for (let i=0; i<totalNavList; i++) {
         const a = navList[i].querySelector("a");
         a.addEventListener("click", function () {
-            for (let j=0; j<totalNavList; j++) {
+            for (let i = 0; i < totalSection; i++) {
+                allSection[i].classList.remove("back-section");
+            }
+            for (let j = 0; j < totalNavList; j++) {
+                if (navList[j].querySelector("a").classList.contains("active")) {
+                    allSection[j].classList.add("back-section");
+                }
                 navList[j].querySelector("a").classList.remove("active");
             }
             this.classList.add("active")
@@ -30,12 +36,22 @@ function showSection(element) {
     document.querySelector("#" + target).classList.add("active")
     }
   
-    /*
+    
 function showSection(element) {
     for (let i = 0; i < totalSection; i++) {
         allSection[i].classList.remove("active");
     }
 
-    const target = element.getAttribute(href).split("#")[1];
+    const target = element.getAttribute("href").split("#")[1];
     document.querySelector("#" + target).classList.add("active")
-} */
+} 
+
+const navTogglerBtn = document.querySelector(".nav-toggler"),
+    aside = document.querySelector(".aside");
+    navTogglerBtn.addEventListener("click", () => {
+    asideSectionTogglerBtn();
+    })
+function asideSectionTogglerBtn() {
+    aside.classList.toggle("open");
+    navTogglerBtn.classList.toggle("open");
+    }
